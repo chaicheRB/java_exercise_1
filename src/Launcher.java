7e3d9b04d246f9d1e66895d3ff9090d51a5f2600
chaiche <77980127+chaicheRB@users.noreply.github.com>
2022-09-20 14:49:15 +0200

    added part 2

---
+++ src/Launcher.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Bienvenue");
+
+        var scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        if (!"quit".equals(input)) {
+            System.out.println("Unknown command");
+        }
+    }
+}
