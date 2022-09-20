1c77052070f10197d5bee4ef969cb0792d066142
chaiche <77980127+chaicheRB@users.noreply.github.com>
2022-09-20 15:22:35 +0200

    added part 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 
 public class Launcher {
+    public static int fibo(int n) {
+        if (0 == n) {
+            return 0;
+        } else if (1 == n) {
+            return 1;
+        } else {
+            return fibo(n-1) + fibo(n-2);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Bienvenue");
 
@@ -9,6 +19,12 @@ public class Launcher {
         String input = scanner.nextLine();
 
         while (!"quit".equals(input)) {
+            if ("fibo".equals(input)) {
+                System.out.println("Choisir n, qui sera l'argument de la suite de Fibonacci");
+                System.out.println(fibo((scanner.nextInt())));
+                break;
+            }
+
             System.out.println("Unknown command");
             input = scanner.nextLine();
         }
